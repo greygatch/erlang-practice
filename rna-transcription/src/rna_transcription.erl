f32ce2cfaa2330e8516389820104541c5f8b9071
Evan Gatchell <egatchell@changehealthcare.com>
2018-07-26 11:32:02 -0500

    progress

--- rna-transcription/src/rna_transcription.erl
+++ rna-transcription/src/rna_transcription.erl
@@ -2,7 +2,7 @@
 
 -export([to_rna/1, test_version/0]).
 
-
-to_rna(Strand) -> undefined.
+-define(Conversions, [{"A", "U"}, {"G", "C"}, {"C", "G"}, {"T", "A"}]).
+to_rna(Strand) -> length(Strand).
 
 test_version() -> 2.
