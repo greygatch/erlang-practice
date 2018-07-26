bc62cf0ad8a07446b56162d84f0a42fc8f6df377
Evan Gatchell <egatchell@changehealthcare.com>
2018-07-25 21:45:24 -0500

    refactor

--- collatz-conjecture/src/collatz_conjecture.erl
+++ collatz-conjecture/src/collatz_conjecture.erl
@@ -7,7 +7,7 @@ steps(N) when N < 1 -> {error, "Only positive numbers are allowed"};
 % what is this line doing?
 steps(N) -> steps(N, 0).
 % base case
-steps(Num, Steps) when Num =:= 1 -> Steps;
+steps(1, Steps) -> Steps;
 % recursive call for even or odd
 steps(Num, Steps) when Num rem 2 =:= 0 -> steps(Num div 2, Steps + 1);
 steps(Num, Steps) -> steps(3 * Num + 1, Steps + 1).
