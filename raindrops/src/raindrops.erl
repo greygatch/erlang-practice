4f4d222eb39e600330720cf50a94445252bd95e2
Evan Gatchell <egatchell@changehealthcare.com>
2018-07-25 22:37:42 -0500

    progress

--- raindrops/src/raindrops.erl
+++ raindrops/src/raindrops.erl
@@ -2,5 +2,11 @@
 
 -export([convert/1]).
 
-convert(N) when N rem 2 =:= 0 -> "Pling";
-convert(N) when N rem 2 =:= 1 -> "Plang".
+convert(N) -> convert(N, 1, []).
+
+convert(N, I, Acc) when I =< trunc(N/2) ->
+    case N rem I of
+        0 -> convert(N, I + 1, [I | Acc]);
+        _ -> convert(N, I + 1, Acc)
+    end;
+convert(N, _I, Acc) -> lists:reverse(Acc) ++ [N].
