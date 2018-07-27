9c5f4f14142949f3d9b67ba15501916cb6107eca
Evan Gatchell <egatchell@changehealthcare.com>
2018-07-26 22:59:38 -0500

    success

--- rna-transcription/src/rna_transcription.erl
+++ rna-transcription/src/rna_transcription.erl
@@ -2,11 +2,9 @@
 
 -export([to_rna/1]).
 
-% {"A", "U"}, {"G", "C"}, {"C", "G"}, {"T", "A"}
-% to_rna(Strand) -> lists:flatten(lists:map(transcribe, Strand)).
-
+has_error(X) -> lists:member(error, lists:map(fun convert/1, X)).
 to_rna(Seq) ->
-  case lists:member(error, lists:map(fun convert/1, Seq)) of
+  case has_error(Seq) of
     true ->
       error;
     false ->
