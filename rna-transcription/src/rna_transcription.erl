f6cf027734a52e2711931f6f05490fd5f968c8bb
Evan Gatchell <egatchell@changehealthcare.com>
2018-07-26 20:30:53 -0500

    progress

--- rna-transcription/src/rna_transcription.erl
+++ rna-transcription/src/rna_transcription.erl
@@ -6,7 +6,10 @@
 
 % to_rna(Strand) -> lists:flatten(lists:map(transcribe, Strand)).
 
-to_rna(Seq) -> lists:member(error, lists:map(fun convert/1, Seq)).
+to_rna(Seq) ->
+  case lists:member(error, lists:map(fun convert/1, Seq)) of ->
+    true.
+  % Clists:member(error, lists:map(fun convert/1, Seq));
 
 convert($G) -> $C;
 convert($C) -> $G;
