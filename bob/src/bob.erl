faaf1fdf746e1aed3f086787bdc7f6b818103b6f
Evan Gatchell <egatchell@changehealthcare.com>
2018-07-27 01:29:17 -0500

    adding challenges

---
+++ bob/src/bob.erl
@@ -0,0 +1,8 @@
+-module(bob).
+
+-export([response/1, test_version/0]).
+
+
+response(String) -> undefined.
+
+test_version() -> 2.
---
+++ hamming/src/hamming.erl
@@ -0,0 +1,8 @@
+-module(hamming).
+
+-export([distance/2, test_version/0]).
+
+
+distance(Strand1, Strand2) -> undefined.
+
+test_version() -> 2.
