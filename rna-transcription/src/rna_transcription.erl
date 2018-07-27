1370b800169a77ccebc4b88eb670eb3ebd01a6f0
Evan Gatchell <egatchell@changehealthcare.com>
2018-07-26 23:00:18 -0500

    success

--- rna-transcription/src/rna_transcription.erl
+++ rna-transcription/src/rna_transcription.erl
@@ -2,7 +2,9 @@
 
 -export([to_rna/1]).
 
-has_error(X) -> lists:member(error, lists:map(fun convert/1, X)).
+has_error(X) ->
+  lists:member(error, lists:map(fun convert/1, X)).
+  
 to_rna(Seq) ->
   case has_error(Seq) of
     true ->
