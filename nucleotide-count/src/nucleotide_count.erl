3f6e7b447cc591580a1d62e5362dd73514e360e2
Evan Gatchell <egatchell@changehealthcare.com>
2018-07-30 12:07:18 -0500

    update tabbing rules for erlang files in atom.

--- nucleotide-count/src/nucleotide_count.erl
+++ nucleotide-count/src/nucleotide_count.erl
@@ -3,12 +3,12 @@
 -export([count/2, nucleotide_counts/1]).
 
 count(Seq, [Nucl]) ->
-  DnaList = "ATCG",
-  case lists:member(Nucl, DnaList) of
-    % wtf?
-    true -> length([N || N <- Seq, N == Nucl]);
-    false -> error("Invalid nucleotide")
-  end.
+    DnaList = "ATCG",
+    case lists:member(Nucl, DnaList) of
+        % wtf?
+        true -> length([N || N <- Seq, N == Nucl]);
+        false -> error("Invalid nucleotide")
+    end.
 
 % wtf
 nucleotide_counts(Seq) -> [{Nucl, count(Seq, Nucl)} || Nucl <- ["A", "T", "C", "G"]].
