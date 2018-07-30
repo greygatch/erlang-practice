94de63951cc87356c091fe57dc591f05e6564c82
Evan Gatchell <egatchell@changehealthcare.com>
2018-07-30 12:02:36 -0500

    commit working solution

--- nucleotide-count/src/nucleotide_count.erl
+++ nucleotide-count/src/nucleotide_count.erl
@@ -3,9 +3,12 @@
 -export([count/2, nucleotide_counts/1]).
 
 count(Seq, [Nucl]) ->
-    case lists:member(Nucl, "ATCG") of
-        true -> length([N || N <- Seq, N =:= Nucl]);
-        false -> error("Invalid nucleotide")
-    end.
+  DnaList = "ATCG",
+  case lists:member(Nucl, DnaList) of
+    % wtf?
+    true -> length([N || N <- Seq, N == Nucl]);
+    false -> error("Invalid nucleotide")
+  end.
 
+% wtf
 nucleotide_counts(Seq) -> [{Nucl, count(Seq, Nucl)} || Nucl <- ["A", "T", "C", "G"]].
