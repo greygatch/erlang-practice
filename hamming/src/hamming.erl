1140dfccd3b44c88e36b987616d29f0033355ce7
Evan Gatchell <egatchell@changehealthcare.com>
2018-09-05 10:16:37 -0500

    refactoring

--- hamming/src/hamming.erl
+++ hamming/src/hamming.erl
@@ -3,10 +3,10 @@
 -export([distance/2, test_version/0]).
 
 
-distance(Strand1, Strand2) when length(Strand1) == length(Strand2) ->
+distance(Strand1, Strand2) when length(Strand1) =:= length(Strand2) ->
   Mutations = [{X, Y} || {X, Y} <- lists:zip(Strand1, Strand2), X =/= Y],
   length(Mutations);
-distance(Strand1, Strand2) when length(Strand1) =/= length(Strand2) ->
+distance(Strand1, Strand2) ->
   {error, "left and right strands must be of equal length"}.
 
 test_version() -> 2.
