f19aad5aa5f4039d635ea1618b034120fb9d7091
Evan Gatchell <egatchell@changehealthcare.com>
2018-09-05 09:44:48 -0500

    initial hamming

--- hamming/src/hamming.erl
+++ hamming/src/hamming.erl
@@ -3,6 +3,8 @@
 -export([distance/2, test_version/0]).
 
 
-distance(Strand1, Strand2) -> undefined.
+distance(Strand1, Strand2) ->
+  Mutations = [{X, Y} || {X, Y} <- lists:zip(Strand1, Strand2), X =/= Y],
+  length(Mutations).
 
 test_version() -> 2.
