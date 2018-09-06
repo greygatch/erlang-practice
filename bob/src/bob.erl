8ac5f1b3385e25dcc083337df393bc1eb69610da
Evan Gatchell <egatchell@changehealthcare.com>
2018-09-06 09:42:11 -0500

    passing tests

--- bob/src/bob.erl
+++ bob/src/bob.erl
@@ -2,7 +2,16 @@
 
 -export([response/1, test_version/0]).
 
+response(String) ->
+  Message = re:replace(String, "(^\\s+)|(\\s+$)", "", [global,{return,list}]),
+  Shouting = (Message =:= string:to_upper(Message)) and (re:run(Message, "[A-Za-z]") =/= nomatch),
+  Asking = lists:suffix("?", Message),
+  Silent = 0 =:= length(string:strip(Message)),
 
-response(String) -> undefined.
+  if Silent -> "Fine. Be that way!";
+    Shouting -> "Whoa, chill out!";
+    Asking -> "Sure.";
+    true -> "Whatever."
+  end.
 
 test_version() -> 2.
